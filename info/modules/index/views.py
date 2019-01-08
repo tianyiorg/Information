a3417096d81ca9009cc97272202740e1412ac1d0
xin <tianyiorg@126.com>
2019-01-08 14:58:48 +0800

    加载favicon.ico图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from . import index_blue
 
@@ -6,3 +6,8 @@ from . import index_blue
 @index_blue.route('/')
 def index():
     return render_template("news/index.html")
+
+
+@index_blue.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file("news/favicon.ico")
